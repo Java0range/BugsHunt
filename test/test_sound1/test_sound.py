291af6cf7d8786292de44f0544949a856a23b532
Nikita Kudaev <javared234@gmail.com>
2025-01-30 19:17:22 +0300

    sound_test

---
+++ test/test_sound1/test_sound.py
@@ -0,0 +1,20 @@
+import pygame as pg
+import sys
+
+pg.init()
+sc = pg.display.set_mode((400, 300))
+
+sound1 = pg.mixer.Sound('Gunshot.wav')
+
+while 1:
+    for i in pg.event.get():
+        if i.type == pg.QUIT:
+            sys.exit()
+
+        elif i.type == pg.MOUSEBUTTONUP:
+            if i.button == 1:
+                sound1.play()
+
+    pg.time.delay(20)
+
+    ivanplanschet @ gmail.com
