af6cf397d94a581faa105af583fa35c40af78ca5
Java0range <javared234@gmail.com>
2025-01-31 19:47:34 +0300

    Fix: name file fix

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
-from Souds import Sounds
+
+from Sounds import Sounds
 
 sounds = Sounds()
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_mode((800, 600))
+    pygame.display.set_mode((800, 540))
