b5147e6d3c6c40f43309f4db2fa097c523786e3d
ivjn <ivanplanschet@gmail.com>
2025-02-02 13:25:48 +0300

    sound_test

--- test/test_sound1/test_sound.py
+++ test/test_sound1/test_sound.py
@@ -1,10 +1,10 @@
 import pygame as pg
 import sys
-
+from Sounds import Sounds
 pg.init()
+so = Sounds()
 sc = pg.display.set_mode((400, 300))
 
-sound1 = pg.mixer.Sound('Gunshot.wav')
 
 while 1:
     for i in pg.event.get():
@@ -13,6 +13,6 @@ while 1:
 
         elif i.type == pg.MOUSEBUTTONUP:
             if i.button == 1:
-                sound1.play()
+                so.play_game_conflict()
 
     pg.time.delay(20)
