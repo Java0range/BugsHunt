b368cd5cac1f5ea52c7a27d88349beb1de619f83
Java0range <javared234@gmail.com>
2025-02-03 17:00:32 +0300

    Add: Sound logic

--- Sounds.py
+++ Sounds.py
@@ -3,22 +3,22 @@ import pygame as pg
 
 class Sounds:
     def __init__(self):
-        self.sound_gun = pg.mixer.Sound('../../data/Gunshot.wav')
+        self.sound_gun = pg.mixer.Sound('./data/Gunshot.wav')
         self.sound_gun.set_volume(0.4)
 
-        self.sound_fall = pg.mixer.Sound('../../data/medlenno_punch.wav')
+        self.sound_fall = pg.mixer.Sound('./data/medlenno_punch.wav')
         self.sound_fall.set_volume(0.4)
 
-        self.start_game = pg.mixer.Sound('../../data/start_game.mp3')
+        self.start_game = pg.mixer.Sound('./data/start_game.mp3')
         self.start_game.set_volume(0.4)
 
-        self.start_conflict = pg.mixer.Sound('../../data/rebound.mp3')
+        self.start_conflict = pg.mixer.Sound('./data/rebound.mp3')
         self.start_conflict.set_volume(0.4)
 
-        self.points_score = pg.mixer.Sound('../../data/points_score.mp3')
+        self.points_score = pg.mixer.Sound('./data/points_score.mp3')
         self.points_score.set_volume(0.4)
 
-        self.game_over = pg.mixer.Sound('../../data/game_over.mp3')
+        self.game_over = pg.mixer.Sound('./data/game_over.mp3')
         self.game_over.set_volume(0.4)
 
     def play_shot(self):
